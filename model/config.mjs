93eecef02a92622c263a75f07cfe12fe5ed1b7ff
nikgabi <nikosgavalakis@gmail.com>
2022-10-28 23:54:23 +0300

    12345

--- model/config.mjs
+++ model/config.mjs
@@ -2,13 +2,12 @@ import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize(
     {
-        host: 'dpg-cdcrc5un6mpsbhf4dni0-a',
-        url: process.env.DATABASE_URL ,
+        hostname: 'dpg-cdcrc5un6mpsbhf4dni0-a',
         port: 5432,
         database: 'booklist',
-        dialect: 'postgres',
         username: 'booklist_user',
         password: 'hpnIzyf1hQUwdQqlG9ZGt6rZIAImGQ9C',
+        Internal Database URL:'postgres://booklist_user:hpnIzyf1hQUwdQqlG9ZGt6rZIAImGQ9C@dpg-cdcrc5un6mpsbhf4dni0-a/booklist'
     });
 
 export default sequelize
